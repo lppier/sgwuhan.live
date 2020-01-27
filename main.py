083362bdf1125ee77f0ff938d8e46078a560a0cf
Pier <madstrum@gmail.com>
2020-01-27 13:41:16 +0800

    first

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import streamlit as st
+
+'''
+# Wuhan Coronavirus in Singapore
+
+Daily Updates of All Places _Confirmed_ Patients Have Visited
+'''
+
+@st.cache
+def load_data():
+    df = pd.read_csv("./data/wuhan_patient_trace.csv")
+    return df
+
+
+df = load_data()
+st.map(df)
+'''
+Based on updates obtained from MOH. https://www.moh.gov.sg/2019-ncov-wuhan
+'''
+if st.checkbox("Show raw data", False):
+    st.write(df)
