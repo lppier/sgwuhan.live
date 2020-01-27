e435eb563e17b9fd1249f3102f76bbd001983713
Pier <madstrum@gmail.com>
2020-01-27 14:49:03 +0800

    editing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 '''
 # Wuhan Coronavirus in Singapore
 
-Daily Updates of All Places _Confirmed_ Patients Have Visited
+Daily Updates of All Places _Confirmed_ Patients Have Visited, including hospitals
 '''
 
 @st.cache
@@ -20,3 +20,5 @@ Based on updates obtained from MOH. https://www.moh.gov.sg/2019-ncov-wuhan
 '''
 if st.checkbox("Show raw data", False):
     st.write(df)
+
+#st.markdown("Written By Pier Lim")
